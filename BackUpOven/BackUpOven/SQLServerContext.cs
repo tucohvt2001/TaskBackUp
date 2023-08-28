d54de2f2a039ac81225741e7e691e3d575ee85f5
tucohvt2001@gmail.com <1234567890>
2023-08-28 20:31:19 +0700

    commit28_08_2023

---
+++ BackUpOven/BackUpOven/SQLServerContext.cs
@@ -0,0 +1,27 @@
+﻿using BackUpOven.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackUpOven
+{
+    internal class SQLServerContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public SQLServerContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        public DbSet<Ovens> Ovens { get; set; }
+        public DbSet<Thistory> Thistory { get; set; }
+    }
+}
