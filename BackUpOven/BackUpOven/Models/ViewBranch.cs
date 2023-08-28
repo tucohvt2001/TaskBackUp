0c29d8f5f21898b70421b8c7674c09011e4b82e8
tucohvt2001@gmail.com <1234567890>
2023-08-29 06:55:55 +0700

    cm29082023

---
+++ BackUpOven/BackUpOven/Models/ViewBranch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackUpOven.Models
+{
+    public partial class ViewBranch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
